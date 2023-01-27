c2fce053346e4c1b32761105650865d8c8231e3f
Katsuya Lex Colón <56055550+klcolon@users.noreply.github.com>
2023-01-27 11:45:22 -0800

    Update pw2_fasta_creator.py

--- fasta_file_creator/pw2_fasta_creator/pw2_fasta_creator.py
+++ fasta_file_creator/pw2_fasta_creator/pw2_fasta_creator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import pandas as pd
 import click
+import numpy as np
 from Bio.Seq import Seq
 
 class pw2_fasta:
-    def __init__(self, fasta, bed, gff):
+    def __init__(self, fasta, gff, bed=None):
         self.fasta = fasta
         self.bed = bed
         self.gff = gff
@@ -28,6 +29,9 @@ class pw2_fasta:
                     temp_gene = []
                     temp_seq = []
                     temp_gene.append(line[1:].replace("\n",""))
+                elif line == "":
+                    temp_seq = ["".join(temp_seq)]
+                    _list.append(temp_gene + temp_seq)
                 else:
                     temp_seq.append(line.replace("\n",""))
             f.close()
@@ -41,6 +45,9 @@ class pw2_fasta:
         """
         Create df from bed file
         """
+        if type(self.bed) == type(None):
+            print('No BED file provided..')
+            return None
         df = pd.read_csv(self.bed, sep = "\t", header=None)
         df.columns = ["Gene", "Start", "End"]
         
@@ -55,40 +62,24 @@ class pw2_fasta:
         
         return df
     
-    def create_fasta(filename, sequences, genenames, biotype):
+    def create_fasta(filename, sequences, id_, product, biotype='mRNA'):
         """
         function to create fasta file
         """
-        #create empty file
-        with open(filename, "w+") as f:
-            f.close()
         #fill fasta file
-        with open(filename, "a") as f:
-            record = 0
+        with open(filename, "w+") as f:
             for i,seq in enumerate(sequences):
-                if i == 0:
-                    f.write(f">{genenames[i]}_{record} dna gene:{genenames[i]}_{record}" \
-                    f" gene_biotype:{biotype}" \
-                    f" transcript_biotype:{biotype} gene_symbol:{genenames[i]}_{record} \n")
-                    f.write(seq + "\n")
-                    record += 1
-                    prev = genenames[i]
-                elif genenames[i] == prev:
-                    f.write(f">{genenames[i]}_{record} dna gene:{genenames[i]}_{record}" \
-                    f" gene_biotype:{biotype}" \
-                    f" transcript_biotype:{biotype} gene_symbol:{genenames[i]}_{record} \n")
-                    f.write(seq + "\n")
-                    record += 1
-                elif genenames[i] != prev:
-                    record = 0 
-                    prev = genenames[i]
-                    f.write(f">{genenames[i]}_{record} dna gene:{genenames[i]}_{record}" \
-                    f" gene_biotype:{biotype}" \
-                    f" transcript_biotype:{biotype} gene_symbol:{genenames[i]}_{record} \n")
-                    f.write(seq + "\n")
-                    record += 1
+                f.write(f">{id_[i]} product:{product[i]}" \
+                f" transcript_biotype:{biotype} \n")
+                f.write(seq + "\n")
         f.close()
-    
+        # reference text file of ids and names
+        with open('reference.csv','w+') as f:
+            for i,x in enumerate(id_):
+                f.write(x +',' + product[i]+'\n')
+        f.close()
+
+        
     def generate_probe_fasta(self, filename, biotype):
         
         """
@@ -104,32 +95,56 @@ class pw2_fasta:
         name = []
         if biotype == "introns":
             print("Generating intron fasta sequences")
+            assert type(self.bed) != type(None), 'Missing BED file'
         elif biotype == "exons":
             print("Generating exon fasta sequences")
+            assert type(self.bed) != type(None), 'Missing BED file'
         else:
-            raise("Check biotype entry. Should be exons or introns.")
+            df_gff = df_gff[df_gff.Feature == 'mRNA'].reset_index(drop=True)
+            print("Generating mRNA fasta sequences using GFF only")
         
-        for gene, end in df_bed[["Gene", "End"]].values:
-            if biotype == "introns":
-                #start of gene exon is the end of the gene intron
-                strand = df_gff[(df_gff.Gene == gene) & (df_gff.Start == end)].Strand.iloc[0]
-                attribute = df_gff[(df_gff.Gene == gene) & (df_gff.Start == end)].Attribute.iloc[0]
-            else:
-                #any the exon end should match with gff end
-                strand = df_gff[(df_gff.Gene == gene) & (df_gff.End == end)].Strand.iloc[0]
-                attribute = df_gff[(df_gff.Gene == gene) & (df_gff.End == end)].Attribute.iloc[0]
-            if "product" in attribute:
-                product = attribute.split(";")[-1].replace("product=", "")
-                name.append(product)
-            elif "Parent" in attribute:
-                parent = attribute.split(";")[-1].replace("Parent=", "")
-                name.append(parent)
-            elif "Name" in attribute:
-                Name = attribute.split(";")[-1].replace("Name=", "")
-                name.append(Name)
-            strand_info.append(strand)
-        df_bed["strand"] = strand_info
-        df_bed["product_name"] = name
+        if biotype != 'mRNA':
+            for gene, end in df_bed[["Gene", "End"]].values:
+                if biotype == "introns":
+                    #start of gene exon is the end of the gene intron
+                    strand = df_gff[(df_gff.Gene == gene) & (df_gff.Start == end)].Strand.iloc[0]
+                    attribute = df_gff[(df_gff.Gene == gene) & (df_gff.Start == end)].Attribute.iloc[0]
+                else:
+                    #any the exon end should match with gff end
+                    strand = df_gff[(df_gff.Gene == gene) & (df_gff.End == end)].Strand.iloc[0]
+                    attribute = df_gff[(df_gff.Gene == gene) & (df_gff.End == end)].Attribute.iloc[0]  
+                #id
+                id_ = attribute.split(";")[0].replace("ID=", "")   
+                #product
+                if "product" in attribute:
+                    product = attribute.split(";")[-1].replace("product=", "")
+                      
+                if 'product' in locals():
+                    name.append([id_, product])
+                else:
+                    name.append([id_, 'NA'])
+                strand_info.append(strand)
+        else:
+            for i in range(len(df_gff)):
+                attribute = df_gff.Attribute.iloc[i]
+                #id
+                id_ = attribute.split(";")[0].replace("ID=", "")   
+                #product
+                if "product" in attribute:
+                    product = attribute.split(";")[-1].replace("product=", "")    
+                if 'product' in locals():
+                    name.append([id_, product])
+                else:
+                    name.append([id_, 'NA'])
+            
+        if biotype != 'mRNA':     
+            df_bed.loc["Strand"] = strand_info
+            df_bed.loc["id"] = np.array(name)[:, 0]
+            df_bed.loc["product"] = np.array(name)[:, 1]
+        else:
+            df_bed = df_gff[['Gene','Start', 'End','Strand']].copy()
+            df_bed["id"] = np.array(name)[:, 0]
+            df_bed["product"] = np.array(name)[:, 1]
         
         #isolate sequences based on bed file
         gene_sequence_final = []
@@ -138,7 +153,7 @@ class pw2_fasta:
             gene_name = df_bed.iloc[i].Gene
             gene_start = df_bed.iloc[i].Start
             gene_end = df_bed.iloc[i].End
-            gene_orientation = df_bed.iloc[i].strand   
+            gene_orientation = df_bed.iloc[i].Strand   
             gene_seq = df_fasta[df_fasta["Gene"] == gene_name].Sequence.iloc[0]
             #cut fragment while accounting for python indexing
             gene_fragment = gene_seq[gene_start-1:gene_end-1]
@@ -147,28 +162,29 @@ class pw2_fasta:
                 gene_fragment = str(Seq(gene_fragment).reverse_complement())
             gene_sequence_final.append(gene_fragment)
             
-        pw2_fasta.create_fasta(filename, gene_sequence_final, df_bed.product_name.values.tolist(), biotype)
+        pw2_fasta.create_fasta(filename, gene_sequence_final, 
+                               df_bed["id"].values.tolist(), df_bed["product"].values.tolist(), biotype)
 
 @click.command()
-@click.option('--fasta','-f', "fasta", help='FASTA File of organism cDNA',required=False, type=str)
-@click.option('--bed','-b', "bed",help='BED file containing genomic coordinates', required=False, type=str)
-@click.option('--gff','-gf', "gff", help='gff file of organism', required=False, type=str)
-@click.option('--biotype','-bio', "biotype",help='what type of molecule is this? (ex.introns or exons)', required=True, type=str)
+@click.option('--fasta','-f', "fasta", help='FASTA File of organism genome',required=False, type=str)
+@click.option('--gff','-gf', "gff", help='gff file of organism', type=str, required=False)
+@click.option('--biotype','-bio', "biotype",help='what type of molecule is this? (ex.introns, exon, mRNA)', required=True, type=str)
 @click.option('--sequences','-s', "sequences",help='Probe sequence csv file', required=False, type=str, default="")
 @click.option('--genenames','-g', "genenames",help='Gene names csv file for probe sequences', required=False, type=str)
 @click.option('--filename','-fi', "filename",help='output file name', required=True, type=str)
+@click.option('--bed','-b', "bed",help='BED file containing genomic coordinates', required=False, type=str)
 
 #overall wrapper function
-def main(fasta, bed, gff, biotype, sequences, genenames, filename):
+def main(fasta, gff, biotype, sequences, genenames, filename, bed=None):
     if sequences == "":
         print("Creating FASTA file...")
-        pw2f = pw2_fasta(fasta, bed, gff)
+        pw2f = pw2_fasta(fasta = fasta, gff=gff, bed=bed)
         pw2f.generate_probe_fasta(filename, biotype)
     else:
         print("Creating FASTA file...")
         sequences = pd.read_csv(sequences, header=None)[0].values.tolist()
         genenames = pd.read_csv(genenames, header=None)[0].values.tolist()
-        pw2_fasta.create_fasta(filename, sequences, genenames, biotype)
+        pw2_fasta.create_fasta(filename, sequences, genenames, genenames, biotype)
     
 if __name__ == '__main__':
     main()
